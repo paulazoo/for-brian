467056faa5ee60386d4b3a95f1b82cd9b9395539
paulazoo <paulazhu@college.harvard.edu>
2021-05-19 19:28:29 -0400

    making pypdf2 work

---
+++ for_brian.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+pdfFileObj = open('example_pdf.pdf','rb')     #'rb' for read binary mode
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+pageObj = pdfReader.getPage(1)          #'9' is the page number
+print(pageObj.extractText())
+
